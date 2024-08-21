ae482b2ef148683de8bc7e136dd52212121d9253
Ryan Richer <richerr@santacruzpl.org>
2024-08-21 12:11:59 -0700

    redid work config with antidote

---
+++ work/zsh antidote/.zsh_plugins.zsh
@@ -0,0 +1,26 @@
+source $ZDOTDIR/.aliases
+fpath+=( $HOME/.cache/antidote/zsh-users/zsh-completions/src )
+fpath+=( $HOME/.cache/antidote/mattmc3/zephyr/plugins/color )
+source $HOME/.cache/antidote/mattmc3/zephyr/plugins/color/color.plugin.zsh
+fpath+=( $HOME/.cache/antidote/mattmc3/zephyr/plugins/directory )
+source $HOME/.cache/antidote/mattmc3/zephyr/plugins/directory/directory.plugin.zsh
+fpath+=( $HOME/.cache/antidote/mattmc3/zephyr/plugins/editor )
+source $HOME/.cache/antidote/mattmc3/zephyr/plugins/editor/editor.plugin.zsh
+fpath+=( $HOME/.cache/antidote/mattmc3/zephyr/plugins/environment )
+source $HOME/.cache/antidote/mattmc3/zephyr/plugins/environment/environment.plugin.zsh
+fpath+=( $HOME/.cache/antidote/mattmc3/zephyr/plugins/history )
+source $HOME/.cache/antidote/mattmc3/zephyr/plugins/history/history.plugin.zsh
+fpath+=( $HOME/.cache/antidote/mattmc3/zephyr/plugins/utility )
+source $HOME/.cache/antidote/mattmc3/zephyr/plugins/utility/utility.plugin.zsh
+fpath+=( $HOME/.cache/antidote/jirutka/zsh-shift-select )
+source $HOME/.cache/antidote/jirutka/zsh-shift-select/zsh-shift-select.plugin.zsh
+fpath+=( $HOME/.cache/antidote/belak/zsh-utils/completion )
+source $HOME/.cache/antidote/belak/zsh-utils/completion/completion.plugin.zsh
+fpath+=( $HOME/.cache/antidote/Aloxaf/fzf-tab )
+source $HOME/.cache/antidote/Aloxaf/fzf-tab/fzf-tab.plugin.zsh
+fpath+=( $HOME/.cache/antidote/zdharma-continuum/fast-syntax-highlighting )
+source $HOME/.cache/antidote/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( $HOME/.cache/antidote/zsh-users/zsh-autosuggestions )
+source $HOME/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( $HOME/.cache/antidote/zsh-users/zsh-history-substring-search )
+source $HOME/.cache/antidote/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
---
+++ work/zsh antidote/.zshrc
@@ -0,0 +1,35 @@
+#!/bin/zsh
+#
+# .zshrc - Zsh file loaded on interactive shell sessions.
+#
+
+eval "$(oh-my-posh init zsh --config $HOME/.config/oh-my-posh/zen.toml)"
+
+# Zsh options.
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# implement moving over and editing words where only alphanumeric characters are considered word characters
+# esentialy will ignore -_/\. characters so it doesnt just the whole path
+# must come before loading syntax highlighting
+# zsh-utils wordchars
+# WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
+# windows terminal word delimiters
+# WORDCHARS=' /\()"'-.,:;<>~!@#$%^&*|+=[]{}~?│'
+autoload -U select-word-style
+select-word-style bash
+
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5A" history-search-backward
+bindkey "^[[1;5B" history-search-forward
+
+# Source zstyles you might use with antidote.
+[[ -e ${ZDOTDIR:-~}/.zstyles ]] && source ${ZDOTDIR:-~}/.zstyles
+
+# Clone antidote if necessary.
+[[ -d ${ZDOTDIR:-~}/.antidote ]] ||
+  git clone https://github.com/mattmc3/antidote ${ZDOTDIR:-~}/.antidote
+
+# Create an amazing Zsh config using antidote plugins.
+source ${ZDOTDIR:-~}/.antidote/antidote.zsh
+antidote load
